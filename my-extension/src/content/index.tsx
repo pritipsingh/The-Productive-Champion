ede9506179767fb018dc2519ae9d3119b609f3d7
ansh mourya <anshmourya657@gmail.com>
2024-02-04 11:54:26 +0530

    feat: add styled restrict page

---
+++ my-extension/src/content/index.tsx
@@ -0,0 +1,36 @@
+import ReactDOM from "react-dom";
+import Restrict from "../components/Restrict";
+
+export const alreadyBlockedWebsites = [
+  "reddit",
+  "facebook",
+  "twitter",
+  "roblox",
+  "instagram",
+  "insta",
+  "netflix",
+  "amazon",
+  "hbo",
+  "hotstar",
+  "messenger",
+];
+console.log("I am the content scripttttt!!!");
+
+chrome.storage.local.get(["websitesToBlock"], function (result) {
+  console.log("star-1");
+  const currentWebsite = window.location.hostname; // or any other method to get the current website
+
+  const websitesList = result.websitesToBlock || [];
+  console.log("from cotent", result.websitesToBlock);
+
+  const restricExist = websitesList.some((item: string) =>
+    currentWebsite.toLowerCase().includes(item.toLowerCase())
+  );
+
+  if (restricExist) {
+    const div = document.createElement("div");
+    document.body.style.overflow = "hidden";
+    document.body.appendChild(div);
+    ReactDOM.render(<Restrict />, div);
+  }
+});
