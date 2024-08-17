b677c45d1bcd20f6c07c24941ef16265ceaa9578
gab <155622983+GabGaitanidis@users.noreply.github.com>
2024-08-17 14:46:15 +0300

    Added button transition

--- my-extension/src/components/CustomButton.tsx
+++ my-extension/src/components/CustomButton.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
 interface CustomButtonProps {
-    label: string;
-    onClick: () => void;
+  label: string;
+  onClick: () => void;
 }
 
-const CustomButton = ({label, onClick}: CustomButtonProps) => {
+const CustomButton = ({ label, onClick }: CustomButtonProps) => {
   return (
-    <button onClick={onClick} type="button" className="text-white w-[40vw] bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-xs px-5 py-2.5 text-center me-2 mb-2">{label}</button>
-  )
-}
+    <button
+      onClick={onClick}
+      type="button"
+      className="text-white w-[40vw] bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-xs px-5 py-2.5 text-center me-2 mb-2 transition-all duration-300 ease-in-out transform hover:scale-105"
+    >
+      {label}
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
